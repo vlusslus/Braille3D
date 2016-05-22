253e7672ae6e9c83ef9da4bcdfdf3f0ffd8b72dc
vlusslus <konstantinovr1@gmail.com>
2016-05-22 20:28:06 +0300

    Initial commit.

---
+++ braille3d/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by vlus on 22.05.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Convert text to printable format!");
+    }
+
+}
